fe25fc57e472b1c53cb635518c4f7107beae7bc1
F Knauss <fknauss@cultureshark.net>
2023-05-20 17:20:28 -0700

    bradfield project

--- school_projects/dicedetect.rb
+++ school_projects/dicedetect.rb
@@ -18,11 +18,7 @@ class Dice
         u = @rolls.length/36.0
         f = m.inject(Hash.new(0)){|p,v| p[v] += u; p}
         return nil if (freq.keys - f.keys).length>0
-        (f.keys.inject(0){|c,v|c+(freq[v]-f[v]).abs}/f.length).round(4)
+        var=f.keys.inject(0){|c,v|c+(freq[v]-f[v])**2}
+        Math.sqrt(var/rolls.length).round(4)
     end
 end
-
-d = Dice.new
-p d.freq.length
-p d.compare(d.dice)
-p d.compare([(1..6).to_a]*2)
